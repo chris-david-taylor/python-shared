a396bb97eeb92c7457864ecac60b405171d1e23d
chris taylor <cdtaylor3@gmail.com>
2022-05-03 12:59:44 +0200

    small bugfix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='toolbox',
+    name='python-shared',
     version='0.0.3',
     author='Mike Huls',
     author_email='mike_huls@hotmail.com',
@@ -16,6 +16,6 @@ setuptools.setup(
         "Bug Tracker": "https://github.com/Muls/toolbox/issues"
     },
     license='MIT',
-    packages=['toolbox'],
+    packages=['python-shared'],
     install_requires=['requests', 'slack-sdk'],
 )
